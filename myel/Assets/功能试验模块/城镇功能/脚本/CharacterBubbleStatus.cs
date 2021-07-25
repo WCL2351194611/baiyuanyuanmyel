b6215eda504999f23c3258a245bc77cdb1b03648
WCL2351194611 <53164863+WCL2351194611@users.noreply.github.com>
2021-07-25 11:24:28 +0800

    意义不明

--- myel/Assets/功能试验模块/城镇功能/脚本/CharacterBubbleStatus.cs
+++ myel/Assets/功能试验模块/城镇功能/脚本/CharacterBubbleStatus.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class CharacterBubbleStatus : MonoBehaviour
 {
+    private CharacterAction action;
+    public int amount=1;
+    public GameObject bubblePrefab;  //气泡预设物
+    private bool start_ = false;
+    private void Start()
+    {
+        action = this.GetComponent<CharacterAction>();
+    }
 
     //ToDo:添加角色场景中气泡按钮
 
@@ -41,4 +49,43 @@ public class CharacterBubbleStatus : MonoBehaviour
     //    }
     //}
     #endregion
+
+        //计算克隆的位置
+    void Calculation_of_position(int amount_)
+    {
+        switch (amount_)
+        {
+            case 1:
+
+                break;
+            default:
+                break;
+        }
+
+       
+
+    }
+        //克隆消息
+    void Clone_Messages()
+    {
+        switch (amount)
+        {
+            case 1:
+
+                amount = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    private void Update()
+    {
+        if (action.groundName =="池塘"&& amount != 0)
+        {
+            start_ = true;
+            Clone_Messages();
+        }
+    }
+
+
 }
